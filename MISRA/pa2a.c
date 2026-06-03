c595e625cc6bfaecc12e350e3f34a1b574f93caf
nagoyakaizen-star <nagoya.kaizen@gmail.com>
2026-06-03 14:27:17 +0900

    Create pa2a.c

---
+++ MISRA/pa2a.c
@@ -0,0 +1,292 @@
+#include<stdio.h>
+
+#define PR(format,value) printf(#value "=%" #format "\t",(value))
+#define NL putchar('\n')
+#define PRINT1(f,x1) PR(f,x1), NL
+#define PRINT2(f,x1, x2) PR(f,x1), PRINT1(f,x2)
+#define PRINT3(f,x1,x2,x3) PR(f,x1), PRINT2(f,x2,x3)
+#define PRINT4(f,x1,x2,x3,x4) PR(f,x1), PRINT3(f,x2,x3,x4)
+
+int a[]={0,1,2,3,4};
+int *p[]={a,a+1,a+2,a+3,a+4};
+int **pp=p;
+int d;
+
+int main()
+{
+    PRINT2(d,a,((int)*a));
+    PRINT3(d,p,((int)*p),((int)**p));
+    PRINT3(d,pp,(int)*pp,(int)**pp);
+    NL;
+
+    pp++; PRINT3(d,pp-p,(int)*pp-a,(int)**pp);
+    *pp++; PRINT3(d,pp-p,(int)*pp-a,(int)**pp);
+    *++pp;PRINT3(d,pp-p,(int)*pp-a,(int)**p);
+    ++*pp; PRINT3(d,pp-p,(int)*pp-a,(int)**pp);
+    NL;
+
+
+pp=p;
+    **pp++; PRINT3(d,pp-p,*pp-a,**pp);
+    *++*pp; PRINT3(d,pp-p,*pp-a,**pp);
+    ++**pp; PRINT3(d,pp-p,*pp-a,**pp);
+}
+/*
+main.c: In function 'main':
+main.c:17:25: warning: format '%d' expects argument of type 'int', but argument 2 has type 'int *' [-Wformat=]
+   17 |     PRINT2(d,a,((int)*a));
+      |                         ^
+main.c:3:34: note: in definition of macro 'PR'
+    3 | #define PR(format,value) printf(#value "=%" #format "\t",(value))
+      |                                  ^~~~~
+main.c:17:5: note: in expansion of macro 'PRINT2'
+   17 |     PRINT2(d,a,((int)*a));
+      |     ^~~~~~
+main.c:18:36: warning: format '%d' expects argument of type 'int', but argument 2 has type 'int **' [-Wformat=]
+   18 |     PRINT3(d,p,((int)*p),((int)**p));
+      |                                    ^
+main.c:3:34: note: in definition of macro 'PR'
+    3 | #define PR(format,value) printf(#value "=%" #format "\t",(value))
+      |                                  ^~~~~
+main.c:18:5: note: in expansion of macro 'PRINT3'
+   18 |     PRINT3(d,p,((int)*p),((int)**p));
+      |     ^~~~~~
+main.c:18:17: warning: cast from pointer to integer of different size [-Wpointer-to-int-cast]
+   18 |     PRINT3(d,p,((int)*p),((int)**p));
+      |                 ^
+main.c:3:59: note: in definition of macro 'PR'
+    3 | #define PR(format,value) printf(#value "=%" #format "\t",(value))
+      |                                                           ^~~~~
+main.c:7:38: note: in expansion of macro 'PRINT2'
+    7 | #define PRINT3(f,x1,x2,x3) PR(f,x1), PRINT2(f,x2,x3)
+      |                                      ^~~~~~
+main.c:18:5: note: in expansion of macro 'PRINT3'
+   18 |     PRINT3(d,p,((int)*p),((int)**p));
+      |     ^~~~~~
+main.c:19:35: warning: format '%d' expects argument of type 'int', but argument 2 has type 'int **' [-Wformat=]
+   19 |     PRINT3(d,pp,(int)*pp,(int)**pp);
+      |                                   ^
+main.c:3:34: note: in definition of macro 'PR'
+    3 | #define PR(format,value) printf(#value "=%" #format "\t",(value))
+      |                                  ^~~~~
+main.c:19:5: note: in expansion of macro 'PRINT3'
+   19 |     PRINT3(d,pp,(int)*pp,(int)**pp);
+      |     ^~~~~~
+main.c:19:17: warning: cast from pointer to integer of different size [-Wpointer-to-int-cast]
+   19 |     PRINT3(d,pp,(int)*pp,(int)**pp);
+      |                 ^
+main.c:3:59: note: in definition of macro 'PR'
+    3 | #define PR(format,value) printf(#value "=%" #format "\t",(value))
+      |                                                           ^~~~~
+main.c:7:38: note: in expansion of macro 'PRINT2'
+    7 | #define PRINT3(f,x1,x2,x3) PR(f,x1), PRINT2(f,x2,x3)
+      |                                      ^~~~~~
+main.c:19:5: note: in expansion of macro 'PRINT3'
+   19 |     PRINT3(d,pp,(int)*pp,(int)**pp);
+      |     ^~~~~~
+main.c:22:45: warning: format '%d' expects argument of type 'int', but argument 2 has type 'long int' [-Wformat=]
+   22 |     pp++; PRINT3(d,pp-p,(int)*pp-a,(int)**pp);
+      |                                             ^
+main.c:3:34: note: in definition of macro 'PR'
+    3 | #define PR(format,value) printf(#value "=%" #format "\t",(value))
+      |                                  ^~~~~
+main.c:22:11: note: in expansion of macro 'PRINT3'
+   22 |     pp++; PRINT3(d,pp-p,(int)*pp-a,(int)**pp);
+      |           ^~~~~~
+main.c:22:25: warning: cast from pointer to integer of different size [-Wpointer-to-int-cast]
+   22 |     pp++; PRINT3(d,pp-p,(int)*pp-a,(int)**pp);
+      |                         ^
+main.c:3:59: note: in definition of macro 'PR'
+    3 | #define PR(format,value) printf(#value "=%" #format "\t",(value))
+      |                                                           ^~~~~
+main.c:7:38: note: in expansion of macro 'PRINT2'
+    7 | #define PRINT3(f,x1,x2,x3) PR(f,x1), PRINT2(f,x2,x3)
+      |                                      ^~~~~~
+main.c:22:11: note: in expansion of macro 'PRINT3'
+   22 |     pp++; PRINT3(d,pp-p,(int)*pp-a,(int)**pp);
+      |           ^~~~~~
+main.c:22:33: error: invalid operands to binary - (have 'int' and 'int *')
+   22 |     pp++; PRINT3(d,pp-p,(int)*pp-a,(int)**pp);
+      |                         ~~~~~~~~^
+      |                         |       |
+      |                         int     int *
+main.c:3:59: note: in definition of macro 'PR'
+    3 | #define PR(format,value) printf(#value "=%" #format "\t",(value))
+      |                                                           ^~~~~
+main.c:7:38: note: in expansion of macro 'PRINT2'
+    7 | #define PRINT3(f,x1,x2,x3) PR(f,x1), PRINT2(f,x2,x3)
+      |                                      ^~~~~~
+main.c:22:11: note: in expansion of macro 'PRINT3'
+   22 |     pp++; PRINT3(d,pp-p,(int)*pp-a,(int)**pp);
+      |           ^~~~~~
+main.c:23:46: warning: format '%d' expects argument of type 'int', but argument 2 has type 'long int' [-Wformat=]
+   23 |     *pp++; PRINT3(d,pp-p,(int)*pp-a,(int)**pp);
+      |                                              ^
+main.c:3:34: note: in definition of macro 'PR'
+    3 | #define PR(format,value) printf(#value "=%" #format "\t",(value))
+      |                                  ^~~~~
+main.c:23:12: note: in expansion of macro 'PRINT3'
+   23 |     *pp++; PRINT3(d,pp-p,(int)*pp-a,(int)**pp);
+      |            ^~~~~~
+main.c:23:26: warning: cast from pointer to integer of different size [-Wpointer-to-int-cast]
+   23 |     *pp++; PRINT3(d,pp-p,(int)*pp-a,(int)**pp);
+      |                          ^
+main.c:3:59: note: in definition of macro 'PR'
+    3 | #define PR(format,value) printf(#value "=%" #format "\t",(value))
+      |                                                           ^~~~~
+main.c:7:38: note: in expansion of macro 'PRINT2'
+    7 | #define PRINT3(f,x1,x2,x3) PR(f,x1), PRINT2(f,x2,x3)
+      |                                      ^~~~~~
+main.c:23:12: note: in expansion of macro 'PRINT3'
+   23 |     *pp++; PRINT3(d,pp-p,(int)*pp-a,(int)**pp);
+      |            ^~~~~~
+main.c:23:34: error: invalid operands to binary - (have 'int' and 'int *')
+   23 |     *pp++; PRINT3(d,pp-p,(int)*pp-a,(int)**pp);
+      |                          ~~~~~~~~^
+      |                          |       |
+      |                          int     int *
+main.c:3:59: note: in definition of macro 'PR'
+    3 | #define PR(format,value) printf(#value "=%" #format "\t",(value))
+      |                                                           ^~~~~
+main.c:7:38: note: in expansion of macro 'PRINT2'
+    7 | #define PRINT3(f,x1,x2,x3) PR(f,x1), PRINT2(f,x2,x3)
+      |                                      ^~~~~~
+main.c:23:12: note: in expansion of macro 'PRINT3'
+   23 |     *pp++; PRINT3(d,pp-p,(int)*pp-a,(int)**pp);
+      |            ^~~~~~
+main.c:24:44: warning: format '%d' expects argument of type 'int', but argument 2 has type 'long int' [-Wformat=]
+   24 |     *++pp;PRINT3(d,pp-p,(int)*pp-a,(int)**p);
+      |                                            ^
+main.c:3:34: note: in definition of macro 'PR'
+    3 | #define PR(format,value) printf(#value "=%" #format "\t",(value))
+      |                                  ^~~~~
+main.c:24:11: note: in expansion of macro 'PRINT3'
+   24 |     *++pp;PRINT3(d,pp-p,(int)*pp-a,(int)**p);
+      |           ^~~~~~
+main.c:24:25: warning: cast from pointer to integer of different size [-Wpointer-to-int-cast]
+   24 |     *++pp;PRINT3(d,pp-p,(int)*pp-a,(int)**p);
+      |                         ^
+main.c:3:59: note: in definition of macro 'PR'
+    3 | #define PR(format,value) printf(#value "=%" #format "\t",(value))
+      |                                                           ^~~~~
+main.c:7:38: note: in expansion of macro 'PRINT2'
+    7 | #define PRINT3(f,x1,x2,x3) PR(f,x1), PRINT2(f,x2,x3)
+      |                                      ^~~~~~
+main.c:24:11: note: in expansion of macro 'PRINT3'
+   24 |     *++pp;PRINT3(d,pp-p,(int)*pp-a,(int)**p);
+      |           ^~~~~~
+main.c:24:33: error: invalid operands to binary - (have 'int' and 'int *')
+   24 |     *++pp;PRINT3(d,pp-p,(int)*pp-a,(int)**p);
+      |                         ~~~~~~~~^
+      |                         |       |
+      |                         int     int *
+main.c:3:59: note: in definition of macro 'PR'
+    3 | #define PR(format,value) printf(#value "=%" #format "\t",(value))
+      |                                                           ^~~~~
+main.c:7:38: note: in expansion of macro 'PRINT2'
+    7 | #define PRINT3(f,x1,x2,x3) PR(f,x1), PRINT2(f,x2,x3)
+      |                                      ^~~~~~
+main.c:24:11: note: in expansion of macro 'PRINT3'
+   24 |     *++pp;PRINT3(d,pp-p,(int)*pp-a,(int)**p);
+      |           ^~~~~~
+main.c:25:46: warning: format '%d' expects argument of type 'int', but argument 2 has type 'long int' [-Wformat=]
+   25 |     ++*pp; PRINT3(d,pp-p,(int)*pp-a,(int)**pp);
+      |                                              ^
+main.c:3:34: note: in definition of macro 'PR'
+    3 | #define PR(format,value) printf(#value "=%" #format "\t",(value))
+      |                                  ^~~~~
+main.c:25:12: note: in expansion of macro 'PRINT3'
+   25 |     ++*pp; PRINT3(d,pp-p,(int)*pp-a,(int)**pp);
+      |            ^~~~~~
+main.c:25:26: warning: cast from pointer to integer of different size [-Wpointer-to-int-cast]
+   25 |     ++*pp; PRINT3(d,pp-p,(int)*pp-a,(int)**pp);
+      |                          ^
+main.c:3:59: note: in definition of macro 'PR'
+    3 | #define PR(format,value) printf(#value "=%" #format "\t",(value))
+      |                                                           ^~~~~
+main.c:7:38: note: in expansion of macro 'PRINT2'
+    7 | #define PRINT3(f,x1,x2,x3) PR(f,x1), PRINT2(f,x2,x3)
+      |                                      ^~~~~~
+main.c:25:12: note: in expansion of macro 'PRINT3'
+   25 |     ++*pp; PRINT3(d,pp-p,(int)*pp-a,(int)**pp);
+      |            ^~~~~~
+main.c:25:34: error: invalid operands to binary - (have 'int' and 'int *')
+   25 |     ++*pp; PRINT3(d,pp-p,(int)*pp-a,(int)**pp);
+      |                          ~~~~~~~~^
+      |                          |       |
+      |                          int     int *
+main.c:3:59: note: in definition of macro 'PR'
+    3 | #define PR(format,value) printf(#value "=%" #format "\t",(value))
+      |                                                           ^~~~~
+main.c:7:38: note: in expansion of macro 'PRINT2'
+    7 | #define PRINT3(f,x1,x2,x3) PR(f,x1), PRINT2(f,x2,x3)
+      |                                      ^~~~~~
+main.c:25:12: note: in expansion of macro 'PRINT3'
+   25 |     ++*pp; PRINT3(d,pp-p,(int)*pp-a,(int)**pp);
+      |            ^~~~~~
+main.c:30:37: warning: format '%d' expects argument of type 'int', but argument 2 has type 'long int' [-Wformat=]
+   30 |     **pp++; PRINT3(d,pp-p,*pp-a,**pp);
+      |                                     ^
+main.c:3:34: note: in definition of macro 'PR'
+    3 | #define PR(format,value) printf(#value "=%" #format "\t",(value))
+      |                                  ^~~~~
+main.c:30:13: note: in expansion of macro 'PRINT3'
+   30 |     **pp++; PRINT3(d,pp-p,*pp-a,**pp);
+      |             ^~~~~~
+main.c:30:37: warning: format '%d' expects argument of type 'int', but argument 2 has type 'long int' [-Wformat=]
+   30 |     **pp++; PRINT3(d,pp-p,*pp-a,**pp);
+      |                                     ^
+main.c:3:34: note: in definition of macro 'PR'
+    3 | #define PR(format,value) printf(#value "=%" #format "\t",(value))
+      |                                  ^~~~~
+main.c:7:38: note: in expansion of macro 'PRINT2'
+    7 | #define PRINT3(f,x1,x2,x3) PR(f,x1), PRINT2(f,x2,x3)
+      |                                      ^~~~~~
+main.c:30:13: note: in expansion of macro 'PRINT3'
+   30 |     **pp++; PRINT3(d,pp-p,*pp-a,**pp);
+      |             ^~~~~~
+main.c:31:37: warning: format '%d' expects argument of type 'int', but argument 2 has type 'long int' [-Wformat=]
+   31 |     *++*pp; PRINT3(d,pp-p,*pp-a,**pp);
+      |                                     ^
+main.c:3:34: note: in definition of macro 'PR'
+    3 | #define PR(format,value) printf(#value "=%" #format "\t",(value))
+      |                                  ^~~~~
+main.c:31:13: note: in expansion of macro 'PRINT3'
+   31 |     *++*pp; PRINT3(d,pp-p,*pp-a,**pp);
+      |             ^~~~~~
+main.c:31:37: warning: format '%d' expects argument of type 'int', but argument 2 has type 'long int' [-Wformat=]
+   31 |     *++*pp; PRINT3(d,pp-p,*pp-a,**pp);
+      |                                     ^
+main.c:3:34: note: in definition of macro 'PR'
+    3 | #define PR(format,value) printf(#value "=%" #format "\t",(value))
+      |                                  ^~~~~
+main.c:7:38: note: in expansion of macro 'PRINT2'
+    7 | #define PRINT3(f,x1,x2,x3) PR(f,x1), PRINT2(f,x2,x3)
+      |                                      ^~~~~~
+main.c:31:13: note: in expansion of macro 'PRINT3'
+   31 |     *++*pp; PRINT3(d,pp-p,*pp-a,**pp);
+      |             ^~~~~~
+main.c:32:37: warning: format '%d' expects argument of type 'int', but argument 2 has type 'long int' [-Wformat=]
+   32 |     ++**pp; PRINT3(d,pp-p,*pp-a,**pp);
+      |                                     ^
+main.c:3:34: note: in definition of macro 'PR'
+    3 | #define PR(format,value) printf(#value "=%" #format "\t",(value))
+      |                                  ^~~~~
+main.c:32:13: note: in expansion of macro 'PRINT3'
+   32 |     ++**pp; PRINT3(d,pp-p,*pp-a,**pp);
+      |             ^~~~~~
+main.c:32:37: warning: format '%d' expects argument of type 'int', but argument 2 has type 'long int' [-Wformat=]
+   32 |     ++**pp; PRINT3(d,pp-p,*pp-a,**pp);
+      |                                     ^
+main.c:3:34: note: in definition of macro 'PR'
+    3 | #define PR(format,value) printf(#value "=%" #format "\t",(value))
+      |                                  ^~~~~
+main.c:7:38: note: in expansion of macro 'PRINT2'
+    7 | #define PRINT3(f,x1,x2,x3) PR(f,x1), PRINT2(f,x2,x3)
+      |                                      ^~~~~~
+main.c:32:13: note: in expansion of macro 'PRINT3'
+   32 |     ++**pp; PRINT3(d,pp-p,*pp-a,**pp);
+      |             ^~~~~~
+
+[Compilation failed with exit code 1]
+*/
